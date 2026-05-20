825b3f161072c6062a91b8d94d3f52b57305916f
Aditya02-P <pradhanaditya0725@gmail.com>
2026-05-20 22:54:49 +0530

    Added initial files along with Home Page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Layout } from '../../components/Layouts/Layout';
+import Section1 from "./Section1.jsx";
+import "../../styles/HomeStyle.css"
+
+const Home = () => {
+  return (
+    <Layout>
+      <Section1 />
+    </Layout>
+  )
+}
+
+export default Home;
